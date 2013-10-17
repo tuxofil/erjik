9292b8f9f1dbaf81d24ba50212dba2d3185522bd
Aleksey Morarash <aleksey.morarash@gmail.com>
2013-10-17 23:53:37 +0300

    remove obsolete scripts

--- hup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec erl -sname "erjik_hupper" \
-    -setcookie erjik_secret_cookie \
-    -noinput \
-    -pa ./ebin \
-    -s erjik hup
-
--- ping.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec erl -sname "erjik_pinger" \
-    -setcookie erjik_secret_cookie \
-    -noinput \
-    -pa ./ebin \
-    -s erjik ping
-
